07f2e89fbd54438b0552b9a503bf8cf475a38aa7
dangisantosh <santoshbadrilal2000@gmail.com>
2023-10-11 18:14:42 +0530

    Final API

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,20 +2,22 @@ import movieModel from "../models/movieModel.js";
 
   export const test = async (req, res) => {
     try {
-
-      const query = { $or: [ { rating: 94 }, { name: "Iron Man" } ] }
-      const stringifiedQuery = JSON.stringify({ query });
-      // the above is the data in your API then stringify it and then push to req.body.query
-
-
-      // get query from req.body as {query} = req.body
-      const finalQuery = JSON.parse(stringifiedQuery)
-      const data = await movieModel.find(finalQuery.query);
-      res.status(200).send({
+      const decodeJSON = (str)=>{
+        let regex = /([{,]\s*)([A-Za-z_][A-Za-z0-9_]*)\s*:/g;
+        let replaceStr = '$1"$2":';
+        return JSON.parse(str.replaceAll(regex, replaceStr));
+    }
+      const {query} = req.body
+      console.log(query)
+      let queryData = decodeJSON(query)
+      console.log(queryData);
+     
+      const data = await movieModel.find( queryData);
+      res.status(200).send(
         data
-      });
+      );
     } catch (error) {
-      console.log(error);
+      console.log(error.message);
       res.status(500).send({
         success: false,
         message: "Something went wrong",
